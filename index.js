b29885ce65d360ec15fc8593fff7017e0eb46c6b
kevit-milan-solanki <milan.solanki.kevit@gmail.com>
2022-12-08 17:39:10 +0530

    day4

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let nav = document.getElementById("slider");
 
-function open(){
-    console.log("hello")
 
+function myFunction() {
+    let x = document.getElementById("slid");
+    if (x.className === "navigation") {
+
+        x.className += " open";
+    } else {
+        x.className = "navigation";
+    }
+
+    let btn = document.getElementById("fixBtn");
+    if (btn.className === "fix-btn") {
+        btn.className += " btnDissappper";
+    } else {
+        btn.className = "fix-btn";
+    }
+
+    let slide = document.getElementById("divsearch");
+
+    if (slide.className === "had-search") {
+        slide.className += " had-searchClose";
+    } else {
+        slide.className = "had-search";
+    }
 }
