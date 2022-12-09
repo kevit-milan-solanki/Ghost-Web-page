5063b85a34226c393b84d5fdf8a9cc12237d60ca
kevit-milan-solanki <milan.solanki.kevit@gmail.com>
2022-12-09 12:13:33 +0530

    Final 1

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 function myFunction() {
     let x = document.getElementById("slid");
     if (x.className === "navigation") {
@@ -9,13 +7,6 @@ function myFunction() {
         x.className = "navigation";
     }
 
-    let btn = document.getElementById("fixBtn");
-    if (btn.className === "fix-btn") {
-        btn.className += " btnDissappper";
-    } else {
-        btn.className = "fix-btn";
-    }
-
     let slide = document.getElementById("divsearch");
 
     if (slide.className === "had-search") {
@@ -23,4 +14,50 @@ function myFunction() {
     } else {
         slide.className = "had-search";
     }
+
+
+    let fixBtn = document.getElementById("fixBtn");
+    if (fixBtn.className === "fix-btn") {
+        fixBtn.className += " BtnGone";
+    } else {
+        fixBtn.className = "fix-btn";
+    }
+
+    let ShowBtn = document.getElementById("Btns")
+    if (ShowBtn.className === "Btn") {
+        ShowBtn.className += " BtnShow";
+    } else {
+        ShowBtn.className = "Btn";
+    }
+
+}
+
+function myFunction2(){
+    let x = document.getElementById("slid");
+    if (x.className === "navigation") {
+
+        x.className += " open";
+    } else {
+        x.className = "navigation";
+    }
+
+    let ShowBtn = document.getElementById("Btns")
+    if (ShowBtn.className === "Btn") {
+        ShowBtn.className += " BtnShow";
+    } else {
+        ShowBtn.className = "Btn";
+    }
+
+    let fixBtn = document.getElementById("fixBtn");
+    if (fixBtn.className === "fix-btn") {
+        fixBtn.className += " BtnGone";
+    } else {
+        fixBtn.className = "fix-btn";
+    }
+
+
+
+
+
+
 }
